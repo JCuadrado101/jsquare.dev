f63f371886b15f04bf82e75e9e6ebed0aa94e9e0
jordancuadrado <jac59155@gmail.com>
2023-01-08 23:18:34 -0500

    added logo to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,18 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { QwikLogo } from '../icons/qwik';
 import styles from './header.css?inline';
+import  logo  from "../../assets/companyLogo.png"
+
 
 export default component$(() => {
   useStylesScoped$(styles);
 
   return (
-    <header>
-        <p>Header Component</p>
-    </header>
+      <header>
+          <div>
+              <img src={logo} alt="company logo" width="60px"/>
+          </div>
+          <div></div>
+      </header>
   );
 });
